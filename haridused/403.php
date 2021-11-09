ff94331a49c75aa7bdacaa0b409b5bef9817437e
opilane <AlekseiTiora>
2021-11-09 10:20:06 +0200

    ülesanne 403 on tehtud

--- haridused/403.php
+++ haridused/403.php
@@ -8,13 +8,14 @@
 <a href="../test.php">tagasi..Ülesanne leht</a>
 <h1>Ülesanne 403</h1>
 <?php
+//ilusad värvi nimed massiivis
 $varv=array('Aqua','Coral','Crimson','FloralWhite','GreenYellow','LavenderBlush','LightSkyBlue','LightSalmon','Lime','Maroon',
     'Moccasin','Orchid','PeachPuff','Sienna','Violet');
-// tsükliga kuvamine värvid massivist
-    for($i = 0; $i <15; $i++){
-        echo"<span style='solor:$varv[$i]'>$varv[$i]</span><br>";
-    }
+//tsükliga kuvame värvid massiivist
+for($i = 0; $i <15; $i++){
+    echo "<span style='color: $varv[$i]'>$varv[$i]</span><br>";
+}
+
 ?>
 </body>
 </html>
-
